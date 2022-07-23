13030d3dc06d2949fd72cc82334466f9fa8b010e
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-07-24 01:17:40 +0900

    상명여고 수정사항.
    1. 마이페이지 제목 빼고 나머지 검은글자로. 제목 색도 바꿈
    2. 메인페이지 버튼 크기 맞추기.
    3. 아이디 찾기에서 버튼들 아래 버튼과 동일하게 만들기
    4. 메인페이지 박스들 띄우기.(마진적용)
    ★★★★4. 시간표 색상 변경.
    학생이 선택해서 바꾸기
    지금 당장은 월요일 첫교시 누르면 랜덤하게 색변경하게 해둠.
    일단은 저렇게해뒀고 추후에 수정해서 팝업창을 띄워서 색을 지정할지. 고민해야함. 하는방법은 저렇게 랜덤하게 바꾸는거나 input:color에 있음.

--- js/main.js
+++ js/main.js
@@ -44,3 +44,12 @@ function main() {
 
 }
 main();
+
+
+function changeColor() {
+  var color = ["#FC5C7D", "#6A82FB", "#38ef7d", "#fffbd5", "#b20a2c", "#CAC531"];
+
+  var num = Math.floor(Math.random() * color.length);
+  var bodyTag = document.getElementsByTagName("td");
+  bodyTag[0].style.backgroundColor = color[num];
+}
